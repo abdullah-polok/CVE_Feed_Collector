df5c7d4cd74e08f7635ab2867d23fb8992b04f33
abdullah-polok <abdullahalrahman23@gmail.com>
2026-03-13 00:00:10 +0300

    Final folder structure for backend and frontend

--- backend/collector.py
+++ backend/collector.py
@@ -7,7 +7,8 @@ response = requests.get("https://services.nvd.nist.gov/rest/json/cves/2.0")
 ## Check if the request was successful 
 if response.status_code == 200:
     cve_data=response.json()
-    # Process the CVE data as needed
-    print(cve_data)
+    for cve in cve_data['vulnerabilities']:
+       print(cve)
 else:
     print(f"Failed to retrieve CVE data. Status code: {response.status_code}")
+
