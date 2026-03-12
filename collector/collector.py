aba411cdd810b4e9896d80029b9823e595a03087
abdullah-polok <abdullahalrahman23@gmail.com>
2026-03-12 23:05:41 +0300

    Use requests and call cve api to get the data in json format

---
+++ collector/collector.py
@@ -0,0 +1,13 @@
+import requests
+
+### CVE API endpoint 
+### get all CVEs from NVD
+response = requests.get("https://services.nvd.nist.gov/rest/json/cves/2.0")
+
+## Check if the request was successful 
+if response.status_code == 200:
+    cve_data=response.json()
+    # Process the CVE data as needed
+    print(cve_data)
+else:
+    print(f"Failed to retrieve CVE data. Status code: {response.status_code}")
